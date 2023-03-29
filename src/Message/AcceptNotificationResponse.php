c6a1e5cdcdd28388fbd8f35b01074040fd0bfeed
tom <tom.matin@digitickets.co.uk>
2023-03-29 10:22:09 +0100

    improved failure messages

--- src/Message/AcceptNotificationResponse.php
+++ src/Message/AcceptNotificationResponse.php
@@ -11,6 +11,22 @@ use Omnipay\Common\Message\AbstractResponse;
 class AcceptNotificationResponse extends AbstractResponse
 {
 
+    const STATUSES = [
+        0 => "successful payment",
+        3 => "The card holder has not completed 3DS, this status will only be seen on the REST API.",
+        4 => "The card issuer has parked the transaction awaiting contact with the customer before proceeding to authorise or decline the transaction",
+        5 => "The transaction was declined by the card owner",
+        20 => "The transaction which was processed was a duplicate. Ensure each transaction has a unique OrderId",
+        30 => "Error executing transaction",
+        40 => "The transaction is currently being processed, please check status again. This status will only be seen from the REST API if called before the JavaScript promise resolves or the Webhook is called",
+        90 => "The access token was revoked while the cardholder was completing 3DS authentication. The transaction was stopped before being sent for processing",
+        99 => "The transaction has been paused pre-execution using the waitPreExecute flag; a call to resume the transaction is expected within 15 minutes",
+        400 => "The request has failed validation by our servers and the transaction has not been submitted to the gateway. Possible causes for this are invalid transaction type or other data in the request",
+        401 => "The access token being used is not valid, the transaction has not been submitted to the gateway. This can be caused if the access token has already been used or the 30 minute expiry time has elapsed",
+        404 => "No access token has been supplied to Connect-E. Transaction has not been submitted to the gateway",
+        500 => "There's been an error submitting the transaction, please check the REST API for the status of the transaction"
+    ];
+
     public function isSuccessful(): bool
     {
         return ($this->data['statusCode'] ?? null) === 0;
@@ -36,7 +52,12 @@ class AcceptNotificationResponse extends AbstractResponse
 
     public function getMessage(): string
     {
-        return $this->data["message"] ?? '';
+        if(getCode != null) {
+            $message = self::STATUSES[$this->getCode()];
+            return ($this->data["message"] ?? '') . " - " . $message;
+        } else {
+            return "message";
+        }
     }
 
     /**
